fb4089c0066905cc9b194846078596f1e4aa0038
Jeff Seymour and Patrick Sullivan <seym1585@gmail.com>
2016-01-06 14:15:40 -0800

    added css styles

--- app.rb
+++ app.rb
@@ -3,10 +3,13 @@ require('sinatra/reloader')
 require('./lib/rock_paper_scis')
 
 get('/') do
+  File.read(File.join('public', 'main.css'))
+  @title = "Rock Paper Scissors"
   erb(:index)
 end
 
 get('/results') do
+  @title = "Results"
   @move = params.fetch('move').rock_paper_scis
   erb(:results)
 end
